ab00ac1a393ff79471a7472956e4b40904dfe88c
Emile Duval <bonjour@emileduval.be>
2015-03-31 01:15:17 +0200

    [build] coming soon page

--- gulpfile.js
+++ gulpfile.js
@@ -39,14 +39,11 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('_sass/main.scss')
-        .pipe($.sass({
-            onError: browserSync.notify
-        }))
-        .pipe($.autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/assets/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'));
+    return $.rubySass('_sass/style.scss')
+            .pipe($.autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+            .pipe(gulp.dest('_site/assets/css'))
+            .pipe(browserSync.reload({stream:true}))
+            .pipe(gulp.dest('assets/css'));
 });
 
 /**
